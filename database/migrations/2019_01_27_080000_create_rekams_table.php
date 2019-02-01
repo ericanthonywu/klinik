f28e6c967979494fe1b7d8555b75fc45cd1bda39
Bryan Wahyu <bryanwahyukp95@gmail.com>
2019-02-01 19:04:12 +0000

    Initial commit

---
+++ database/migrations/2019_01_27_080000_create_rekams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRekamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rekams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rekams');
+    }
+}
